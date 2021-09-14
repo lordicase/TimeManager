d09bf885430021719bb0c872181eb117fcfd9aa5
FIRST_NAME LAST_NAME <jkopczynski96@gmail.com>
2021-09-14 13:06:22 +0200

    added RecycleViev Adapter

--- app/src/main/java/com/example/timemanager/MainActivity.java
+++ app/src/main/java/com/example/timemanager/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.timemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static RecyclerView recycleView;
+    String project[], time[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recycleView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        project = getResources().getStringArray(R.array.Project);
+        time = getResources().getStringArray(R.array.Time);
+
+        Project_Adapter projectAdapter = new Project_Adapter(this, project,time);
+        recycleView.setAdapter(projectAdapter);
+        recycleView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/timemanager/Project_Adapter.java
+++ app/src/main/java/com/example/timemanager/Project_Adapter.java
@@ -1,18 +1,20 @@
 package com.example.timemanager;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Project_RecyclerViev extends RecyclerView.Adapter<Project_RecyclerViev.ViewHolder> {
+public class Project_Adapter extends RecyclerView.Adapter<Project_Adapter.ViewHolder> {
 
     String project[], time[];
     Context context;
 
-    public Project_RecyclerViev(Context ct, String pro[], String tm[]){
+    public Project_Adapter(Context ct, String pro[], String tm[]){
     context = ct;
     project=pro;
     time=tm;
@@ -21,22 +23,30 @@ public class Project_RecyclerViev extends RecyclerView.Adapter<Project_RecyclerV
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_project, parent, false);
+        return new ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.project_tv.setText(project[position]);
+        holder.time_tv.setText(time[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return project.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView project_tv, time_tv;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            project_tv = itemView.findViewById(R.id.textView1);
+            time_tv = itemView.findViewById(R.id.textView2);
         }
     }
 }
