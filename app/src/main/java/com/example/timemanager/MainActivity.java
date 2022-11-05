21a36a5075d2f8ba57de46f45706cc5069322c76
Jakub Kopczynski <jkopczynski96@gmail.com>
2022-11-05 11:20:24 +0100

    zaliczenie przedmiotu

--- app/src/main/java/com/example/timemanager/MainActivity.java
+++ app/src/main/java/com/example/timemanager/MainActivity.java
@@ -37,9 +37,6 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.backClick = backClick;
     }
 
-
-
-
     public static int getStartedPosition() {
         return startedPosition;
     }
@@ -65,8 +62,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_projects, R.id.navigation_all_tasks, R.id.navigation_all_stat)
                 .build();
